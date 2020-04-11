dfa2c37f0b5796b623857e8d35c36f4bda793f3c
VanjaPetrushevski <vanjo.petrusevski@gmail.com>
2020-04-11 13:09:26 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,17 +1,52 @@
 class Student {
-	String index;
-	String firstName;
-	String lastName;
-	List<int> labPoints=new ArrayList<int>();
-	//TODO constructor
+    String index;
+    String firstName;
+    String lastName;
+    List<Integer> labPoints=new ArrayList<Integer>();
 
-	//TODO seters & getters
+    public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.labPoints = labPoints;
+    }
 
-	public double getAverage() {
-		//TODO
-	}
+    public String getIndex() {
+        return index;
+    }
 
-	public boolean hasSignature() {
-		//TODO
-	}
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getAverage() {
+        int vkupno=0;
+        for(int i=0;i<this.labPoints.size();i++){
+            vkupno=vkupno+this.labPoints.get(i); }
+        return vkupno/this.labPoints.size();
+    }
+    public boolean hasSignature() {
+        if(labPoints.size()>8){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
